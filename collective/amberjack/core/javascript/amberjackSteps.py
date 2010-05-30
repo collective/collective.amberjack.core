ccff16fbea45b87ad7e3b3763ef06ee194e798f8
Luca Fabbri <luca@keul.it>
2010-05-30 09:31:46 +0000

    Added some more info in dosctring
    git-svn-id: https://svn.plone.org/svn/collective/collective.amberjack.core/trunk@118416 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/amberjack/core/javascript/amberjackSteps.py
+++ collective/amberjack/core/javascript/amberjackSteps.py
@@ -3,6 +3,15 @@ from zope.component import getUtility
 from collective.amberjack.core.interfaces import IMicroStepsManager
 
 class AmberjackSteps(BrowserView):
+    """Generate a Javascript structure like this:
+    
+    AjStandardSteps = {
+        selectorKind: selector,
+        ...
+    }
+    
+    Differents selectorKind value use in different way the given (optional) selector.    
+    """
     def __call__(self, context, request):
         microstepsmanager = getUtility(IMicroStepsManager)
         js = 'AjStandardSteps = {'
